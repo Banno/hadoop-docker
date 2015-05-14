0ab956c2b543d21631f0011a96738e56fbc874dc
Zach Cox <zach.cox@banno.com>
2015-05-14 11:40:25 -0500

    banno-specific hdfs stuff for druid

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,19 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 # altering the core-site configuration
 sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
-
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
+
+######################################################################################
+# commands banno druid usage:
+
+#have to turn safe mode off before we can write to hdfs
+/usr/local/hadoop/bin/hdfs dfsadmin -safemode leave
+
+#create /druid hdfs dir for druid-setup tests
+/usr/local/hadoop/bin/hadoop fs -mkdir /druid && /usr/local/hadoop/bin/hadoop fs -chown druid /druid
+######################################################################################
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
