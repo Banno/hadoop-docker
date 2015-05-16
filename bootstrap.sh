eb7a1a6381ceefbf0f9d8ecedefbe5c99c5b8a1f
Zach Cox <zach.cox@banno.com>
2015-05-16 09:52:12 -0500

    hadoop 2.0.0-mr1-cdh4.7.0 seems to work

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
+# $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
-
-rm /tmp/*.pid
+# rm /tmp/*.pid
 
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+# cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
-# altering the core-site configuration
-sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+sed s/HOSTNAME/$HOSTNAME/ /etc/hadoop/conf/core-site.xml.template > /etc/hadoop/conf/core-site.xml
+sed s/HOSTNAME/$HOSTNAME/ /etc/hadoop/conf/mapred-site.xml.template > /etc/hadoop/conf/mapred-site.xml
 
-service sshd start
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
+mkdir -p /tmp/hadoop-hdfs/dfs/name /tmp/hadoop-hdfs/dfs/data
+chown -R hdfs:hdfs /tmp/hadoop-hdfs/dfs/name /tmp/hadoop-hdfs/dfs/data
+su - hdfs -c 'hdfs namenode -format -force'
+
+service hadoop-hdfs-namenode start
+service hadoop-hdfs-datanode start
+
+su - hdfs -c 'hadoop fs -mkdir /tmp'
+su - hdfs -c 'hadoop fs -mkdir /user'
+su - hdfs -c 'hadoop fs -chmod -R 1777 /tmp'
+su - hdfs -c 'hadoop fs -mkdir -p /var/lib/hadoop-hdfs/cache/mapred/mapred/staging && hadoop fs -chown -R mapred /var/lib/hadoop-hdfs/cache/mapred'
+su - hdfs -c 'hadoop fs -chmod 1777 /var/lib/hadoop-hdfs/cache/mapred/mapred/staging'
+su - hdfs -c 'hadoop fs -mkdir /tmp/mapred/system && hadoop fs -chown mapred:hadoop /tmp/mapred/system'
+su - hdfs -c 'hadoop fs -mkdir -p /tmp/hadoop-mapred/mapred && hadoop fs -chown -R mapred /tmp/hadoop-mapred/mapred && hadoop fs -chmod 1777 /tmp/hadoop-mapred/mapred'
+
+service hadoop-0.20-mapreduce-jobtracker start
+service hadoop-0.20-mapreduce-tasktracker start
 
 ######################################################################################
 # commands banno druid usage:
 
 #have to turn safe mode off before we can write to hdfs
-/usr/local/hadoop/bin/hdfs dfsadmin -safemode leave
+# /usr/local/hadoop/bin/hdfs dfsadmin -safemode leave
 
 #create /druid hdfs dir for druid-setup tests
-/usr/local/hadoop/bin/hadoop fs -mkdir /druid && /usr/local/hadoop/bin/hadoop fs -chown druid /druid
+su - hdfs -c 'hadoop fs -mkdir /druid && hadoop fs -chown druid /druid'
 ######################################################################################
 
 if [[ $1 == "-d" ]]; then
--- build-push.sh
+++ build-push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t registry.banno-internal.com/hadoop-docker:2.0.0-cdh4.4.0 .
-docker push registry.banno-internal.com/hadoop-docker:2.0.0-cdh4.4.0
+docker build -t registry.banno-internal.com/hadoop-docker:2.0.0-cdh4.4.0b .
+# docker push registry.banno-internal.com/hadoop-docker:2.0.0-cdh4.4.0b
