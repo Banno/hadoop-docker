e24a148da4ac081f9af0dd4e6fa7cd50bb34e245
Zach Cox <zach.cox@banno.com>
2015-07-22 15:59:18 -0500

    include latest

--- build-push.sh
+++ build-push.sh
@@ -4,3 +4,6 @@ TAG="2.0.0-mr1-cdh4.4.0"
 
 docker build -t registry.banno-internal.com/hadoop-docker:$TAG .
 docker push registry.banno-internal.com/hadoop-docker:$TAG
+
+docker build -t registry.banno-internal.com/hadoop-docker:latest .
+docker push registry.banno-internal.com/hadoop-docker:latest
